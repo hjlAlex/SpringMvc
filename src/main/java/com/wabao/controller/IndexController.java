6e244dee6c59da81aabd3f30b012dcedd32e35ba
huangjiedragon <huangjiedragon@126.com>
2017-11-22 13:53:45 +0800

    新增事务调试及其注意点

--- src/main/java/com/wabao/controller/IndexController.java
+++ src/main/java/com/wabao/controller/IndexController.java
@@ -1,25 +1,37 @@
 /** 广州哇宝信息技术有限公司 */
 package com.wabao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.wabao.constant.AjaxStatus;
+import com.wabao.dao.GameDao;
+import com.wabao.entity.Game;
 import com.wabao.entity.User;
 import com.wabao.json.AjaxJson;
 import com.xiaoleilu.hutool.json.JSONUtil;
 
 @Controller
 public class IndexController {
+	@Autowired
+	private GameDao gameDao;
+
+	@Transactional
 	@RequestMapping("/")
 	public ModelAndView index(User user) {
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("channels", "user");
 		mav.addObject("fmap", user.getName());
 		mav.addObject("pageBean", user.getAge());
+		Game game = new Game();
+		game.setName("mybatis还好");
+		// game.setUrl("mybatisurl");
+		gameDao.save(game);
 		return mav;
 	}
 
--- src/test/java/com/wabao/test/SpringJunitTest.java
+++ src/test/java/com/wabao/test/SpringJunitTest.java
@@ -3,17 +3,23 @@ package com.wabao.test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
+import org.hibernate.internal.SessionFactoryImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wabao.dao.GameDao;
+import com.wabao.entity.Game;
 import com.wabao.service.GameService;
 import com.xiaoleilu.hutool.json.JSONUtil;
 
 /**
+ * SpringJUnit环境下使用事务注解@Transactional默认都是回滚的,如果想要不会滚,使用注解@Rollback(false)
  * 
  * @since 2017年11月21日 下午2:17:41
  * @author Administrator
@@ -25,6 +31,12 @@ public class SpringJunitTest {
 	@Autowired
 	private GameService gameService;
 
+	@Autowired
+	private GameDao gameDao;
+
+	@Autowired
+	private SessionFactoryImpl sessionFactoryImpl;
+
 	@Test
 	public void gameTestFind() {
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -33,9 +45,25 @@ public class SpringJunitTest {
 				.findPage("from Game where id <= :id", params, 2, 2)));
 	}
 
+	@Transactional
 	@Test
 	public void gameTestSave() {
+		Game game = new Game();
+		game.setName("mybatis还好");
+		gameDao.save(game);
+		System.out.println(1);
+	}
 
+	@Test
+	public void gameTestAdd() {
 		System.out.println(gameService.addGame());
+	}
+
+	@Test
+	public void sessionFactoryTest() {
+		Properties properties = sessionFactoryImpl.getProperties();
+		for (Object key : properties.keySet()) {
+			System.out.println(key + ":" + properties.get(key));
+		}
 	}
 }
